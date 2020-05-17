7be0624211ad792b4dd09d5d6bdec7cf0eda3e07
Railson25 <railsonsantiago123@gmail.com>
2020-05-17 19:10:02 -0300

    CRUD Frontend

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>    
+        <strong>
+            Copyright &copy; 2020
+            <a href='https://github.com/Railson25' target='_blank'> Railson</a>
+        </strong>
+    </footer>
+)
